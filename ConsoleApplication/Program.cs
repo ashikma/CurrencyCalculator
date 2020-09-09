f36fd5efa09244c1e19c804bbf7d841f75d89767
ashikma <alexshikma@gmail.com>
2020-09-09 11:50:00 +0300

    09.09.2020

---
+++ ConsoleApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Currency c = new Currency();
+            c.Name = "Dollar";
+            c.Unit = 0;
+        }
+    }
+}
---
+++ CurrencyCalculator/Models/Currency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CurrencyCalculator.Models
+{
+    public class Currency
+    {
+        int unit;
+
+        public string Name { get; set; }
+        public int Unit
+        {
+            get { return this.unit; }
+            set { if (this.unit <= 0)
+                    this.unit = value;
+            }
+        }
+
+    }
+}
